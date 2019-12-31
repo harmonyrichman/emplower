7501673fc6a7e2435907e804355b8ef55bfefbe6
Harmony Richman <hmrichman@gmail.com>
2019-12-30 21:53:40 -0500

    made checklist boxes animation uniform

--- client/components/article23a.js
+++ client/components/article23a.js
@@ -49,27 +49,27 @@ class Article23a extends Component {
               <li>
                 New York State encourages the employment of persons with prior
                 convictions.
-                <h4>
+                <h3>
                   Do you live in New York? Your state wants employers to provide
                   opportunities to residents who have criminal convictions
-                </h4>
+                </h3>
               </li>
 
               <li>
                 The specific duties and responsibilities necessarily related to
                 the license or employment sought.
-                <h4>
+                <h3>
                   What does the job actually entail? What are the day to day
                   responsibilities of the job? Does it require any special
                   skills?
-                </h4>
+                </h3>
               </li>
 
               <li>
                 The bearing, if any, the criminal offense(s) for which the
                 person was convicted will have on their ability to perform the
                 job duties.
-                <h4>
+                <h3>
                   Does the job relate to the charge? Could the conviction make
                   you less suitable for that particlar job? For example, someone
                   with a gun charge would not be eligible for a security
@@ -77,49 +77,49 @@ class Article23a extends Component {
                   convicted of a burglary might not be suitable for a job as a
                   bank teller. Don't worry though- there are other jobs that
                   will be a good fit for you!
-                </h4>
+                </h3>
               </li>
 
               <li>
                 The time that has elapsed since the occurance of the criminal
                 offense(s).
-                <h4>
+                <h3>
                   How long has it been since the crime was committed? Remember-
                   this is different from when you might have received a
                   conviction. Showing that time has passed since the committed
                   crime creates space between the decision you made then and the
                   person you are now.
-                </h4>
+                </h3>
               </li>
 
               <li>
                 The age of the person at the time of the occurance of the
                 criminal offense(s).
-                <h4>
+                <h3>
                   How old were you when the crime was committed? It is
                   scientific fact that our brains are not fully developed as
                   adolescents, so being able to address how your decision making
                   skills and mentality has developed and matured since then is
                   key.
-                </h4>
+                </h3>
               </li>
 
               <li>
                 The seriousness of the offense(s).
-                <h4>
+                <h3>
                   It is important that different charges are examined
                   differently. If you have a nonviolent conviction, or if there
                   is context to the charge that you think is appropriate for a
                   hiring mangager to know, then let them know. The
                   criminalization of poverty is very real- and the
                   responsibility of that is on the structure of our society.
-                </h4>
+                </h3>
               </li>
 
               <li>
                 Any information produced by the person, or produced on behalf of
                 the person, in regard to their rehabilitation and good conduct.
-                <h4>
+                <h3>
                   What have you done since then? This is an opportunity for you
                   to take control of your own story. What did you learn through
                   your experience? Did you go back to school or start/finish a
@@ -128,19 +128,19 @@ class Article23a extends Component {
                   offense? This is your opportunity to show the upward direction
                   you are striving for, and that you are not interested in
                   making decisions that will push you backwards.
-                </h4>
+                </h3>
               </li>
 
               <li>
                 The legitimate interest of the public agency or private employer
                 in protecting property, and the safety and welfare of the
                 general public.
-                <h4>
+                <h3>
                   Will hiring you put anyone at risk? This includes other
                   employees and customers. This is a big one! You want to
                   demonstrate that you will be a benefit to the company, and not
                   a risk.
-                </h4>
+                </h3>
               </li>
             </ol>
           </p>
